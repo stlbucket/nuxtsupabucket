73ea25e5580f5818159c8fb2c89294ec29903aa4
Kevin Burkett <stlbucket@gmail.com>
2023-07-27 14:07:46 -0700

    sync

--- apps/docs/pages/.vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import MyButton from '@nuxtsupabucket/ui/components/Button.vue'
-
-export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('MyButton', MyButton)
-  },
-}
